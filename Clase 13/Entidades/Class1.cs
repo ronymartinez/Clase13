05825907886a9ec568e509f5876dd6001ba434ad
ronymartinez <ronymartinez.c@gmail.com>
2021-10-18 22:18:45 -0300

    Agregar archivos de proyecto.

---
+++ Clase 13/Entidades/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Class1 : Class2, IMiInterfaz
+    {
+        int Propiedad { get { return 4; } set => throw new NotImplementedException(); }
+        
+        List<int> 
+        public string Saludar()
+        {
+            return "oli";
+        }
+    }
+}
---
+++ Ejercicios/Biblioteca/Lapiz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Lapiz: IAcciones
+    {
+        private float tamanioMina;
+
+        public Lapiz(float tamanioMina)
+        {
+            this.tamanioMina = tamanioMina;
+        }
+
+        ConsoleColor IAcciones.Color
+        {
+            get { return ConsoleColor.Gray; }
+            set { throw new NotImplementedException(); }
+        }
+        float IAcciones.Escribir(string texto)
+        {
+
+        }
+        bool IAcciones.Recargar(int unidades)
+        {
+            throw new NotImplementedException();
+        }
+        EscrituraWrapper IAcciones.Escribir(string texto)
+        {
+            if (tamanioMina >= texto.Length *0.1 )
+            {
+                tamanioMina -= texto.Length * 0.1f;
+                return new 
+            }
+        }
+    }
+}
